4891de4b63b7bbb097c4b2a70cc2c38a73d8a170
miaocansky <1535474309@qq.com>
2022-02-16 16:29:30 +0800

    composer

--- src/Math/Math.php
+++ src/Math/Math.php
@@ -1,9 +1,16 @@
 <?php
+
 namespace Math;
 class Math
 {
-    public function sum($a,$b){
+    public function sum($a, $b)
+    {
 
-        return $a+$b;
+        return $a + $b;
+    }
+
+    public function reduce($a, $b)
+    {
+        return $a - $b;
     }
 }
